716d33a95be93ef075e4e952cd146e685ef0db20
Simon Elvery <simon@elvery.net>
2017-09-29 14:19:59 +1000

    Starts here

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * The nuclear (or perhaps opposite thereof) option.
+ * Remove all the preview guff and reset everything back how it is in production.
+ */
+module.exports = function() {
+  var $ctx = $('span[id^="CTX-"]');
+  var scripts = $ctx
+    .find('script')
+    .filter(function() {
+      var $this = $(this),
+        txt = $this.text(),
+        src = $this.attr('src');
+
+      return (
+        (src && src.match(/joo\.js/)) ||
+        (src && src.match(/coremedia/)) ||
+        txt.match(/coremedia/) ||
+        txt.match(/joo\.classLoader/)
+      );
+    })
+    .remove();
+
+  $ctx.children().unwrap();
+  $ctx.remove();
+};
